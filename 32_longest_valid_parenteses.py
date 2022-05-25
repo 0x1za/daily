e22f23d0f65080315d073e8ef1e7326f2a3f22db
Mwiza Simbeye <mwizasimbeye@gmail.com>
2022-05-25 13:29:47 +0200

    Checkpoint

--- 32_longest_valid_parenteses.py
+++ 32_longest_valid_parenteses.py
@@ -1,9 +1,21 @@
 class Solution:
+    OPENING = '('
     def longestValidParentheses(self, s: str) -> int:
-        output = None
-        return output
+        found = 0
+        stack = []
+        if len(s) != 0:
+            for item in s:
+                if item == self.OPENING:
+                    stack.append(item)
+                else:
+                    if len(stack) != 0:
+                        stack.pop()
+                        found += 2
+            return found
+        else:
+            return 0
 
 a = Solution()
-a.longestValidParentheses("(((())))")
+print(a.longestValidParentheses("(((())))"))
 
 
